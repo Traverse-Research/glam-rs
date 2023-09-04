67f435413367b176ee9b8d6d180fc23502cea63e
Sylvester Hesp <sylvester@traverseresearch.nl>
2023-09-04 11:54:55 +0200

    Changed unit tests for euler angles on quaternions to go through the full -180..=180 range of angles

--- tests/euler.rs
+++ tests/euler.rs
@@ -44,14 +44,40 @@ macro_rules! assert_approx_angle {
     }};
 }
 
+/// Helper to get the 'canonical' version of a `Quat`. We define the canonical of quat q as:
+/// * `q`, if q.w > epsilon
+/// * `-q`, if q.w < -epsilon
+/// * `(0, 0, 0, 1) otherwise
+/// The rationale is that q and -q represent the same rotation, and any (_, _, _, 0) respresent no rotation at all.
+trait CanonicalQuat: Copy {
+    fn canonical(self) -> Self;
+}
+
+macro_rules! impl_canonical_quat {
+    ($t:ty) => {
+        impl CanonicalQuat for $t {
+            fn canonical(self) -> Self {
+                match self {
+                    _ if self.w >= 1e-5 => self,
+                    _ if self.w <= -1e-5 => -self,
+                    _ => <$t>::from_xyzw(0.0, 0.0, 0.0, 1.0),
+                }
+            }
+        }
+    };
+}
+
+impl_canonical_quat!(glam::Quat);
+impl_canonical_quat!(glam::DQuat);
+
 macro_rules! impl_3axis_test {
     ($name:ident, $t:ty, $quat:ident, $euler:path, $U:path, $V:path, $W:path, $vec:ident) => {
         glam_test!($name, {
             let euler = $euler;
             assert!($U != $W); // First and last axis must be different for three axis
-            for u in (-176..=176).step_by(44) {
-                for v in (-88..=88).step_by(44) {
-                    for w in (-176..=176).step_by(44) {
+            for u in (--180..=180).step_by(15) {
+                for v in (-180..=180).step_by(15) {
+                    for w in (-180..=180).step_by(15) {
                         let u1 = (u as $t).to_radians();
                         let v1 = (v as $t).to_radians();
                         let w1 = (w as $t).to_radians();
@@ -63,15 +89,13 @@ macro_rules! impl_3axis_test {
 
                         // Test if the rotation is the expected
                         let q2: $quat = $quat::from_euler(euler, u1, v1, w1).normalize();
-                        assert_approx_eq!(q1, q2, 1e-5);
+                        assert_approx_eq!(q1.canonical(), q2.canonical(), 1e-5);
 
                         // Test angle reconstruction
                         let (u2, v2, w2) = q1.to_euler(euler);
                         let q3 = $quat::from_euler(euler, u2, v2, w2).normalize();
 
-                        assert_approx_angle!(u1, u2, 1e-4 as $t);
-                        assert_approx_angle!(v1, v2, 1e-4 as $t);
-                        assert_approx_angle!(w1, w2, 1e-4 as $t);
+                        assert_approx_eq!(q1.canonical(), q3.canonical(), 1e-5);
 
                         assert_approx_eq!(q1 * $vec::X, q3 * $vec::X, 1e-4);
                         assert_approx_eq!(q1 * $vec::Y, q3 * $vec::Y, 1e-4);
@@ -95,7 +119,7 @@ macro_rules! impl_all_quat_tests_three_axis {
 }
 
 mod euler {
-    use super::AngleDiff;
+    use super::{AngleDiff, CanonicalQuat};
     use glam::*;
     type ER = EulerRot;
 
